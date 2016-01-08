d089f5cae8613808e3f99ff8bd1fbc3e7e883436
Ryan Hollister <ryan.hollister@q2ebanking.com>
2016-01-08 08:48:56 -0600

    Only run plugin during test and only on files in the test directory

--- addon.js
+++ addon.js
@@ -1,35 +1,30 @@
-var checker   = require('ember-cli-version-checker');
+var checker = require('ember-cli-version-checker');
 var inlineTemplateCompiler = require('./index');
 var path = require('path');
 // for ember-cli always use the bower version of the ember template compiler
-var compiler; 
+var compiler;
 
 module.exports = {
-  name: 'broccoli-ember-inline-template-compiler',
-  init: function() {
-    compiler = require(path.join(this.project.root, './bower_components/ember/ember-template-compiler.js'));
-  }, 
-  shouldSetupRegistryInIncluded: function() {
-    return !checker.isAbove(this, '0.2.0');
-  },
-
-  setupPreprocessorRegistry: function(type, registry) {
-    registry.add('js', {
-      name: 'broccoli-ember-inline-template-compiler',
-      ext: 'js',
-      toTree: function(tree) {
-        return inlineTemplateCompiler(tree, { 
-          compiler: compiler
+    name: 'broccoli-ember-inline-template-compiler',
+    init: function() {
+        compiler = require(path.join(this.project.root, './bower_components/ember/ember-template-compiler.js'));
+    },
+    _setupPreprocessorRegistry: function(type, registry) {
+        registry.add('js', {
+            name: 'broccoli-ember-inline-template-compiler',
+            ext: 'js',
+            toTree: function(tree) {
+                return inlineTemplateCompiler(tree, {
+                    compiler: compiler
+                });
+            }
         });
-      }
-    });
-  },
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-
-    if (this.shouldSetupRegistryInIncluded()) {
-      this.setupPreprocessorRegistry('parent', app.registry);
+    },
+    included: function(app) {
+        this._super.included.apply(this, arguments);
+        var enabled = (app.options.emberInlineTemplateCompiler && app.options.emberInlineTemplateCompiler.enabled) || false;
+        if (enabled) {
+            this._setupPreprocessorRegistry('parent', app.registry);
+        }
     }
-
-  }
 };
--- index.js
+++ index.js
@@ -5,46 +5,55 @@ var Filter = require('broccoli-filter');
 module.exports = EmberInlineTemplatePrecompiler;
 EmberInlineTemplatePrecompiler.prototype = Object.create(Filter.prototype);
 EmberInlineTemplatePrecompiler.prototype.constructor = EmberInlineTemplatePrecompiler;
-function EmberInlineTemplatePrecompiler (inputTree, options) {
-  if (!(this instanceof EmberInlineTemplatePrecompiler)) return new EmberInlineTemplatePrecompiler(inputTree, options);
 
-  this.inputTree = inputTree;
-  this.compiler = options && options.compiler || require('ember-template-compiler');
-  this.inlineTemplateRegExp = /precompileTemplate\(['"](.*)['"]\)/;
-  // Used for replacing the original variable declaration to satisfy JSHint.
-  // For example, removes `var precompileTemplate = Ember.Handlebars.compile;`.
-  this.precompileTemplateVarRegex = /var precompileTemplate =.*\r?\n/g;
+function EmberInlineTemplatePrecompiler(inputTree, options) {
+    if (!(this instanceof EmberInlineTemplatePrecompiler)) return new EmberInlineTemplatePrecompiler(inputTree, options);
+
+    this.inputTree = inputTree;
+    this.compiler = options && options.compiler || require('ember-template-compiler');
+    this.inlineTemplateRegExp = /precompileTemplate\(['"](.*)['"]\)/;
+    // Used for replacing the original variable declaration to satisfy JSHint.
+    // For example, removes `var precompileTemplate = Ember.Handlebars.compile;`.
+    this.precompileTemplateVarRegex = /var precompileTemplate =.*\r?\n/g;
 }
 
 EmberInlineTemplatePrecompiler.prototype.extensions = ['js'];
 EmberInlineTemplatePrecompiler.prototype.targetExtension = 'js';
 
-EmberInlineTemplatePrecompiler.prototype.processFile = function (srcDir, destDir, relativePath) {
-  var self = this;
+EmberInlineTemplatePrecompiler.prototype.processFile = function(srcDir, destDir, relativePath) {
+    var self = this;
+    var outputPath = this.getDestFilePath(relativePath);
+    var inputString = fs.readFileSync(srcDir + '/' + relativePath, {
+        encoding: 'utf8'
+    });
 
-  var inputString = fs.readFileSync(srcDir + '/' + relativePath, { encoding: 'utf8' });
-  var outputPath = this.getDestFilePath(relativePath);
-  var outputString = processTemplates().replace(this.precompileTemplateVarRegex, '');
-
-  fs.writeFileSync(destDir + '/' + outputPath, outputString, { encoding: 'utf8' });
-
-  function processTemplates() {
-    var nextIndex;
-    getNextIndex();
-
-    while (nextIndex > -1) {
-      var match = inputString.match(self.inlineTemplateRegExp);
-      var template = "Ember.Handlebars.template(" + self.compiler.precompile(match[1], false) + ")";
-
-      inputString = inputString.replace(match[0], template);
-
-      getNextIndex();
+    if (relativePath.indexOf('/tests/') === -1) {
+        var outputString = inputString;
+    } else {
+        var outputString = processTemplates().replace(this.precompileTemplateVarRegex, '');
     }
 
-    function getNextIndex() {
-      nextIndex = inputString.search(self.inlineTemplateRegExp);
-    }
+    fs.writeFileSync(destDir + '/' + outputPath, outputString, {
+        encoding: 'utf8'
+    });
 
-    return inputString;
-  }
+    function processTemplates() {
+        var nextIndex;
+        getNextIndex();
+
+        while (nextIndex > -1) {
+            var match = inputString.match(self.inlineTemplateRegExp);
+            var template = "Ember.Handlebars.template(" + self.compiler.precompile(match[1], false) + ")";
+
+            inputString = inputString.replace(match[0], template);
+
+            getNextIndex();
+        }
+
+        function getNextIndex() {
+            nextIndex = inputString.search(self.inlineTemplateRegExp);
+        }
+
+        return inputString;
+    }
 };
